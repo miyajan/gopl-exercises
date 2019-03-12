5eaa7ca6b59c8009af3c926c81c82f619f270c08
miyajan <miyajan777@gmail.com>
2019-03-12 09:03:57 +0900

    Add exercise 1.6

---
+++ chapter-1/exercise-1.6/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"math"
+	"math/rand"
+	"os"
+	"time"
+)
+
+var red = color.RGBA{R: 0xff, A: 0xff}
+var green = color.RGBA{G: 0xff, A: 0xff}
+var blue = color.RGBA{B: 0xff, A: 0xff}
+var palette = []color.Color{color.Black, red, green, blue}
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+	lissajous(os.Stdout)
+}
+
+func lissajous(out io.Writer) {
+	const (
+		cycles  = 5
+		res     = 0.001
+		size    = 100
+		nframes = 64
+		delay   = 8
+	)
+	freq := rand.Float64() * 3.0
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			index := uint8(math.Ceil(t/(2*math.Pi)))%3 + 1
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), index)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim)
+}
